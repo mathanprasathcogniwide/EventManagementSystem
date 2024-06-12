af8498e30173c193ae44d12bdd6759d3d79ff473
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-06-12 17:26:29 +0530

    Add files via upload

---
+++ EventREST/src/main/java/com/maddy/EventREST/model/User.java
@@ -0,0 +1,17 @@
+package com.maddy.EventREST.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Automatically send ID's for us..
+    private int id;
+    private String username;
+    private String password;
+
+}
---
+++ EventREST/src/test/java/com/maddy/EventREST/EventRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maddy.EventREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

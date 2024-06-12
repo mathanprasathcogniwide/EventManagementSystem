12f0415f759f82879b36395b981715b0c88be4ed
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-06-12 17:24:54 +0530

    Delete EventREST directory

--- EventREST/src/main/java/com/maddy/EventREST/repo/RegistrationDataRepo.java
+++
@@ -1,8 +0,0 @@
-package com.maddy.EventREST.repo;
-
-import com.maddy.EventREST.model.RegistrationData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationDataRepo extends JpaRepository<RegistrationData, String> {
-
-}

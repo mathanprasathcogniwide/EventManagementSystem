ec052d0b00ac400372227d3edacade01ff0b5517
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-06-13 12:21:17 +0530

    Delete EventREST directory

--- EventREST/src/main/java/com/maddy/EventREST/dao/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.maddy.EventREST.dao;
-
-import com.maddy.EventREST.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepo extends JpaRepository<Admin, Integer> {
-
-    Admin findByUsername(String username);
-}

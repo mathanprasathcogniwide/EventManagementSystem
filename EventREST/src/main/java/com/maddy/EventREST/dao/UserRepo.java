d17b1f6fba80bfeb63a4faa27e63b15e79951e10
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-06-14 17:39:51 +0530

    Delete EventREST directory

--- EventREST/src/main/java/com/maddy/EventREST/dao/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.maddy.EventREST.dao;
-
-import com.maddy.EventREST.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-    User findByUsername(String username);
-}

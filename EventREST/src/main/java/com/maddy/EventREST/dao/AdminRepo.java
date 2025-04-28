7ca7237dab37556e2b4e5b6d1413ac1f2c28dcce
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2025-04-28 18:06:09 +0530

    Update AdminRepo.java

--- EventREST/src/main/java/com/maddy/EventREST/dao/AdminRepo.java
+++ EventREST/src/main/java/com/maddy/EventREST/dao/AdminRepo.java
@@ -1,7 +1,7 @@
 package com.maddy.EventREST.dao;
 
 import com.maddy.EventREST.model.Admin;
-import org.springframework.date.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepo extends JpaRepository<Admin, Integer> {
 
